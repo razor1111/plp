b260a050e02b57573f541a707a6771dd47ab5570
Thiago Santos Borges <31315184+thiagos98@users.noreply.github.com>
2017-10-06 14:18:59 -0400

    Update trabalho-pp2.lisp

--- trabalho-pp2.lisp
+++ trabalho-pp2.lisp
@@ -1,12 +1,14 @@
 ;2a questão da lista
 (defun fatorial (n) "Calc fatorial de um numero"
-		(if (<= n 0)
-			1
-			(* n (fatorial (- n 1)))
-		)
+	(if (<= n 0)
+		1
+		(* n (fatorial (- n 1)))
 	)
+)
 
 (defun somatorio (n,i) "Realiza somatorio"
-	(+ (/ (fatorial i) (+ i 1)) (- i 1)) 
+	(if (< i n)
+		(+ (+ (/ (fatorial i) (+ i 1)) (- i 1)) (somatorio n (+ i 1)))
+		0 
 	)
-
+)
